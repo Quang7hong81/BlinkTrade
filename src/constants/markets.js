c40f2e80e046f3a4442090db5d8583379c6e8048
THONG NGO <38796360+QuangThong1@users.noreply.github.com>
2021-07-16 10:45:53 +0700

    Initialize dependabot/npm_and_yarn/ini-1.3.7

---
+++ src/constants/markets.js
@@ -0,0 +1,6 @@
+export const BLINK_BTCBRL = 'BLINK:BTCBRL';
+export const BLINK_BTCUSD = 'BLINK:BTCUSD';
+export const BLINK_BTCCLP = 'BLINK:BTCCLP';
+export const BLINK_BTCVND = 'BLINK:BTCVND';
+export const UOL_USDBRT = 'UOL:USDBRT';
+export const UOL_USDBRL = 'UOL:USDBRL';
---
+++ src/constants/messages.js
@@ -0,0 +1,66 @@
+import * as reqs from './requestTypes';
+import * as actions from './actionTypes';
+import { msgToAction } from '../util/utils';
+
+export const MsgActionReq = {
+  1:   [actions.HEARTBEAT, reqs.TestReqID],
+  BE:  [actions.LOGIN, reqs.UserReqID],
+  V:   [actions.MD_FULL_REFRESH, reqs.MDReqID],
+  x:   [actions.SECURITY_LIST, reqs.SecurityReqID],
+  e:   [actions.SECURITY_STATUS_SUBSCRIBE, reqs.SecurityStatusReqID],
+  D:   [actions.ORDER_SEND, reqs.ClOrdID],
+  F:   [actions.ORDER_CANCEL, reqs.ClOrdID],
+  U2:  [actions.BALANCE, reqs.BalanceReqID],
+  U6:  [actions.WITHDRAW_REQUEST, reqs.WithdrawReqID],
+  U4:  [actions.ORDER_HISTORY, reqs.OrdersReqID],
+  U18: [actions.DEPOSIT_REQUEST, reqs.DepositReqID],
+  U20: [actions.DEPOSIT_METHODS, reqs.DepositMethodReqID],
+  U24: [actions.WITHDRAW_CONFIRM, reqs.WithdrawReqID],
+  U26: [actions.WITHDRAW_LIST, reqs.WithdrawListReqID],
+  U28: [actions.BROKER_LIST, reqs.BrokerListReqID],
+  U30: [actions.DEPOSIT_LIST, reqs.DepositListReqID],
+  U32: [actions.TRADE_HISTORY, reqs.TradeHistoryReqID],
+  U34: [actions.LEDGER_LIST, reqs.LedgerListReqID],
+  U42: [actions.POSITIONS, reqs.PositionReqID],
+  U70: [actions.WITHDRAW_CANCEL, reqs.WithdrawCancelReqID],
+  B0:  [actions.DEPOSIT_PROCESS, reqs.ProcessDepositReqID],
+  B2:  [actions.CUSTOMER_LIST, reqs.CustomerListReqID],
+  B4:  [actions.KYC_REQUEST, reqs.CustomerReqID],
+  B6:  [actions.WITHDRAW_PROCESS, reqs.ProcessWithdrawReqID],
+  B8:  [actions.KYC_VERIFY, reqs.VerifyCustomerReqID],
+};
+
+export const MsgActionRes = {
+  0:   [actions.HEARTBEAT, reqs.TestReqID],
+  BF:  [actions.LOGIN, reqs.UserReqID],
+  W:   [actions.MD_FULL_REFRESH, reqs.MDReqID],
+  X:   [actions.MD_INCREMENT, reqs.MDReqID],
+  8:   [actions.EXECUTION_REPORT, reqs.ClOrdID],
+  y:   [actions.SECURITY_LIST, reqs.SecurityReqID],
+  f:   [actions.SECURITY_STATUS_SUBSCRIBE, reqs.SecurityStatusReqID],
+  U3:  [actions.BALANCE, reqs.BalanceReqID],
+  U7:  [actions.WITHDRAW_REQUEST, reqs.WithdrawReqID],
+  U5:  [actions.ORDER_HISTORY, reqs.OrdersReqID],
+  U9:  [actions.WITHDRAW_REFRESH, reqs.ClOrdID],
+  U19: [actions.DEPOSIT_REQUEST, reqs.DepositReqID],
+  U21: [actions.DEPOSIT_METHODS, reqs.DepositMethodReqID],
+  U23: [actions.DEPOSIT_REFRESH, reqs.ClOrdID],
+  U25: [actions.WITHDRAW_CONFIRM, reqs.WithdrawReqID],
+  U27: [actions.WITHDRAW_LIST, reqs.WithdrawListReqID],
+  U29: [actions.BROKER_LIST, reqs.BrokerListReqID],
+  U31: [actions.DEPOSIT_LIST, reqs.DepositListReqID],
+  U33: [actions.TRADE_HISTORY, reqs.TradeHistoryReqID],
+  U35: [actions.LEDGER_LIST, reqs.LedgerListReqID],
+  U43: [actions.POSITIONS, reqs.PositionReqID],
+  U71: [actions.WITHDRAW_CANCEL, reqs.WithdrawCancelReqID],
+  U79: [actions.WITHDRAW_COMMENT, reqs.WithdrawReqID],
+  B1:  [actions.DEPOSIT_PROCESS, reqs.ProcessDepositReqID],
+  B3:  [actions.CUSTOMER_LIST, reqs.CustomerListReqID],
+  B5:  [actions.KYC_REQUEST, reqs.CustomerReqID],
+  B9:  [actions.KYC_VERIFY, reqs.VerifyCustomerReqID],
+  B7:  [actions.WITHDRAW_PROCESS, reqs.ProcessWithdrawReqID],
+  B11: [actions.CUSTOMER_REFRESH, ''],
+};
+
+export const ActionMsgReq = msgToAction(MsgActionReq);
+export const ActionMsgRes = msgToAction(MsgActionRes);
